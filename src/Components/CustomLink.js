5725f3fbebc442a9166becec531aa48e368d4a0d
Arman <papyandeveloper@gmail.com>
2022-09-28 22:07:09 +0400

    test

--- src/Components/CustomLink.js
+++ src/Components/CustomLink.js
@@ -4,7 +4,7 @@ import {Link,useMatch}from 'react-router-dom'
 function CustomLink({children,to, ...props}) {
     const match = useMatch(to)
     return (
-        <Link
+         <Link
             to={to}
             style={{
                 color:match?"#d76f6f":"green"
